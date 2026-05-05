896a85d2744d7e9c6484a584d3f55ea3f55835cf
Dr.Dexter <superjay6000@gmail.com>
2026-05-05 05:39:47 +0100

    chore: add src/validators.js for benchmark seed

---
+++ src/validators.js
@@ -0,0 +1,11 @@
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function validatePhone(phone) {
+  const re = /^\+?[1-9]\d{1,14}$/;
+  return re.test(phone);
+}
+
+module.exports = { validatePhone };
